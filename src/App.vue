63a17e12dcac36654596f727112bf2c4f3851ff1
Mizu <psdk55052004@gmail.com>
2022-08-10 10:58:26 +0800

    新增 檔案、資料夾結構

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+    <PagesNav></PagesNav>
   <router-view />
 </template>
+
+<script>
+import PagesNav from './components/layout/navbar/PagesNav.vue'
+export default {
+  components: {
+    PagesNav
+  }
+}
+</script>
 
 <style lang="scss">
 @import "@/assets/stylesheets/all.scss";
--- src/views/ExamManage.vue
+++ src/views/ExamManage.vue
@@ -1,15 +1,12 @@
 <template>
-  <HelloWorld></HelloWorld>
   <IsLoading v-model:active="isLoading"></IsLoading>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 //! import emitter from '@/utils/emitter.js'
 export default {
   components: {
-    HelloWorld
   },
 
   data () {
