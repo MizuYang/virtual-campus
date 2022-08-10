44cd2cdac184e12b4c7ef2635ae6eb97e3097d29
Mizu <psdk55052004@gmail.com>
2022-08-10 13:53:29 +0800

    新增 功能按鈕列表

---
+++ src/components/examManage/FunctionBtnList.vue
@@ -0,0 +1,61 @@
+<template>
+  <ul class="funBtnList d-flex bg-blue-light border list-unstyled">
+    <li class="border-dotted-end hover-border-bottom">
+      <a href="javascript:;" class="text-decoration-none d-inline-block px-4 pe-1 py-1">
+        <span>新增考試</span>
+      </a>
+    </li>
+    <li class="border-dotted-end hover-border-bottom">
+      <a href="javascript:;" class="text-decoration-none d-inline-block px-4 pe-1 py-1">
+        <span>命題紙下載</span>
+      </a>
+    </li>
+    <li class="border-dotted-end hover-border-bottom">
+      <a href="javascript:;" class="text-decoration-none d-inline-block px-4 pe-1 py-1">
+        <span>索引管理</span>
+      </a>
+    </li>
+    <li class="border-dotted-end hover-border-bottom">
+      <a href="javascript:;" class="text-decoration-none d-inline-block px-4 pe-1 py-1">
+        <span>匯入英文上機期中考</span>
+      </a>
+    </li>
+    <li class="border-dotted-end hover-border-bottom">
+      <a href="javascript:;" class="text-decoration-none d-inline-block px-4 pe-1 py-1">
+        <span>匯入英文上機期末考</span>
+      </a>
+    </li>
+    <li class="border-dotted-end hover-border-bottom">
+      <a href="javascript:;" class="text-decoration-none d-inline-block px-4 pe-1 py-1">
+        <span>歷程</span>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  watch: {
+  },
+
+  methods: {
+  },
+
+  mounted () {
+  }
+
+}
+</script>
+
+<style lang='scss' scope>
+@import '@/assets/stylesheets/helpers/pages/examManage/FunctionBtnList.scss'
+</style>
