2d0041f2c96fd7ed46f1afef1af59e55b9c9a7e0
Mizu <psdk55052004@gmail.com>
2022-08-12 09:34:16 +0800

    新增 線上作答選單內部 item 的元件

---
+++ src/components/examManage/addExam/addExam-items/addExam_AnswerOnline/menu-items/PreviewExamPaper.vue
@@ -0,0 +1,26 @@
+<template>預覽試卷</template>
+
+<script>
+
+export default {
+  components: {
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  watch: {
+  },
+
+  methods: {
+  },
+
+  mounted () {
+  }
+
+}
+</script>
+
+<style lang='scss' scope></style>
